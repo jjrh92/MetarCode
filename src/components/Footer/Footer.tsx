c9af757f5b5caea2d78f63ce02a2af52738ee936
Julio Reyes <juliojesusreyes@gmail.com>
2024-12-17 22:50:00 +0100

    Updating footer

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,16 +1,16 @@
 import React from "react";
-import Link from "next/link";
 
 function Footer() {
   return (
     <section className="flex justify-center items-center bg-black h-20 px-8 md:px-20">
       <div className="flex flex-col items-center justify-center text-white">
-        <Link
-          className="text-xl text-center"
-          href={"https://www.julioreyes.dev"}
+        <a
+          href="https://www.julioreyes.dev"
+          target="_blank"
+          rel="noopener noreferrer"
         >
           www.julioreyes.dev
-        </Link>
+        </a>
       </div>
     </section>
   );
